807d2b14a376ac353f60c194fc7f8987212f821b
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2023-12-12 08:24:05 +0000

    password-generator-app
    first app developed using Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
