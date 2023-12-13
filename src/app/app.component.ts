2f2e2a600d2f7500fa3a5100f4eafb47e125dd81
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2023-12-13 16:01:30 +0000

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatIconModule],
+  imports: [CommonModule, RouterOutlet, MatIconModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
