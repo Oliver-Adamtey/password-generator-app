c98b396dd1bc2e56445ac9a69f1edde39ee04560
Lezcano Tech  <153436452+Oliver-Adamtey@users.noreply.github.com>
2023-12-17 13:46:38 +0000

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-// import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import {Clipboard} from '@angular/cdk/clipboard';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
